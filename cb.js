ded10544e046fd08b666189ab55623eb84ddb16c
yilin <bebest10@gmail.com>
2019-07-03 01:16:58 +0800

    update with new jisilu api & upgrade npm packages

--- cb.js
+++ cb.js
@@ -1,6 +1,8 @@
 
 import superagent from 'superagent';
+import cheerio from 'cheerio'
 import * as util from './util';
+const cpn = require('./cpn.json')
 
 // "bond_id": "110031",
 // "bond_nm": "航信转债",
@@ -12,6 +14,7 @@ import * as util from './util';
 // "cpn_desc": "第一年0.20%、第二年0.50%、第三年1.00%、第四年1.50%、第五年1.50%、 第六年1.60%",
 // "redeem_price": "107.000",
 // "year_left": "3.378",
+// "premium_rt": "-15.19%"
 // "ytm_rt": "2.47%",
 // "ytm_rt_tax": "1.85%",
 // "full_price": "102.380"
@@ -19,7 +22,7 @@ import * as util from './util';
 
 const
     cbUrl = "https://www.jisilu.cn/data/cbnew/cb_list/?___jsl=LST___t=",
-    IrRegex = /\w+\s*(\d*\.?\d*)%/ig;
+    IrRegex = /\w+\s*(\d*\.?\d*)%/ig
 
 function parseIr(str) {
     let result = [];
@@ -43,42 +46,70 @@ function calcBondValue(irs, redeemPrice, yearLeft, discount = 0.04) {
 
     return { cb_value: util.trunc(cbValue, 3), bond_value: util.trunc(bondValue, 3) };
 }
+function calcIssueDate(maturityDate) {
+    let year = parseInt(maturityDate.substr(0, 4), 10)
+    return (year - 6).toString() + maturityDate.substr(4, 10)
+}
+async function getIrs(bondId) {
+    return await superagent
+        .get(`https://www.jisilu.cn/data/convert_bond_detail/${bondId}`)
+        .then(res => {
+            const $ = cheerio.load(res.text)
+            return parseIr($('#cpn_desc').text())
+        })
+}
 
 if (require.main === module) {
-    superagent
+    const getCDs = superagent
         .post(cbUrl + new Date().getTime())
         .send("btype=C")
         .send("rp=50")
         .set('Accept', 'application/json')
         .then((res) => {
-            let newRows = res.body.rows.map((row) => {
+            return res.body.rows.map((row) => {
                 let cell = row.cell;
-                let item = {
+                // the order is important, please place in order of the headers
+                return {
                     id: cell.bond_id,
                     name: cell.bond_nm,
-                    issue_dt: cell.issue_dt,
+                    issue_dt: calcIssueDate(cell.maturity_dt), //cell.issue_dt,
                     maturity_dt: cell.maturity_dt,
-                    irs: parseIr(cell.cpn_desc),
+                    irs: undefined, //parseIr(cell.cpn_desc),
                     year_left: Number(cell.year_left),
                     // convert_value: Number(cell.convert_value),
                     redeem_price: Number(cell.redeem_price),
-                    price: Number(cell.full_price)
+                    price: Number(cell.full_price),
+                    cb_value: undefined,
+                    bond_value: undefined,
+                    rating_cd: cell.rating_cd,
+                    premium_rt: cell.premium_rt,
+                    ytm_rt: cell.ytm_rt
                 };
-
-                let { cb_value: cbValue, bond_value: bondValue } = calcBondValue(item.irs, item.redeem_price, item.year_left);
-                item['cb_value'] = cbValue;
-                item['bond_value'] = bondValue;
-
-                delete item.irs;
-                delete item.year_left;
-
-                return item;
             });
-
-            newRows.sort((x, y) => x.issue_dt.localeCompare(y.issue_dt));
-            util.toMarkdownTable(newRows);
-
         });
+
+    (async () => {
+        let items = await getCDs
+        for (let item of items) {
+            if (cpn[item.id] === undefined) {
+                item.irs = await getIrs(item.id)
+                console.log(item.id, ',', item.irs)
+            } else {
+                item.irs = cpn[item.id]
+            }
+
+            let { cb_value: cbValue, bond_value: bondValue } = calcBondValue(item.irs, item.redeem_price, item.year_left);
+            item['cb_value'] = cbValue;
+            item['bond_value'] = bondValue;
+
+            delete item.irs;
+            delete item.year_left;
+        }
+
+        items.sort((x, y) => x.issue_dt.localeCompare(y.issue_dt));
+        util.toMarkdownTable(items);
+    })()
+
 } else {
     exports.parseIr = parseIr;
 }
--- util.ts
+++ util.ts
@@ -15,10 +15,10 @@ function toMarkdownTable_jsl(rows: Array<Object>): void {
 
     let headers = Object.keys(rows[0]);
 
-    console.log('| 代码 | 名称 | 发行日 | 到期日 | 赎回价 | 现价 | 转债价值 | 债券价值 |');
-    console.log('| :--: | :--: | :--: | :--: | ---: | ---: | ---: | ---: |');
+    console.log('| 序号 | 代码 | 名称 | 发行日 | 到期日 | 赎回价 | 现价 | 转债价值 | 纯债价值 | 评级 | 溢价率 | 到期收益 |');
+    console.log('| :--: | :--: | :--: | :--: | :--: | ---: | ---: | ---: | ---: | :--: | ---: | ---: |');
 
-    rows.forEach(row => {
+    rows.forEach((row, idx) => {
         let cells = headers.map(header => {
             if (header === "id") {
                 return `[${row[header]}](https://www.jisilu.cn/data/convert_bond_detail/${row[header]})`;
@@ -27,15 +27,49 @@ function toMarkdownTable_jsl(rows: Array<Object>): void {
         });
 
         // special period
-        if (row["issue_dt"] > within6Months && !within6MonthsAdded) {
-            console.log('| 6个月内 |');
-            within6MonthsAdded = true;
-        }
-        if (row["issue_dt"] > within2Years && !within2YearsAdded) {
-            console.log('| 2年内 |');
-            within2YearsAdded = true;
+        if (row["issue_dt"] > within6Months) {
+            // within 6 months
+            if (!within6MonthsAdded) {
+                within6MonthsAdded = true;
+                console.log('| 6个月内 |');
+            }
+            if (cells[5] < cells[7]) {
+                // 现价 < 纯债价值
+                cells[5] = `**${cells[5]}**`;
+            } else if (cells[5] > 131) {
+                // 现价 > 131
+                cells[5] = `~~${cells[5]}~~`;
+            }
+        } else if (row["issue_dt"] > within2Years) {
+            // 6 months ~ 2 years
+            if (!within2YearsAdded) {
+                console.log('| 2年内 |');
+                within2YearsAdded = true;
+            }
+            if (cells[5] < cells[7]) {
+                // 现价 < 纯债价值
+                cells[5] = `**${cells[5]}**`;
+            } else if (cells[5] < 100) {
+                // 现价 < 100
+                cells[5] = `*${cells[5]}*`;
+            } else if (cells[5] > 130) {
+                cells[5] = `~~${cells[5]}~~`;
+            }
+        } else {
+            // above 2 years
+            if (cells[5] < cells[7]) {
+                // 现价 < 纯债价值
+                cells[5] = `**${cells[5]}**`;
+            } else if (cells[5] < 101) {
+                // 现价 < 101
+                cells[5] = `*${cells[5]}*`;
+            } else if (cells[5] > cells[6] || cells[5] > 131) {
+                // 现价 > 转债价值 || 现价 > 131
+                cells[5] = `~~${cells[5]}~~`;
+            }
         }
 
+        cells.unshift(idx + 1)
         console.log(`| ${cells.join(' | ')} |`);
     });
     console.log(`updated @ ${new Date().toLocaleDateString()}`)
